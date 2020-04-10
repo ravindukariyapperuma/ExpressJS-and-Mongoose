effdc8c7055358d82d0923fa1fb5ccd3eaf9b8f5
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-11 03:00:26 +0530

    add post method

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 'use strict';
 const express = require( 'express' );
 const app = express();
+app.use(express.json());
 app.use(express.static(__dirname));
+
 app.get('/', (req, res, next) => {
     res.sendFile('index.html');
+});
+
+const users = [];
+
+app.post('/users', (req, res, next) => {
+    const user = req.body;
+    user.birthday = new Date(user.birthday);
+    user.id = Date.now();
+    users.push(user);
+    res.json(user);
 });
 
 app.listen(3000, err => {
